f0d38745b79220d31dc4a61cce8ca39f01fe14d8
thomas.chiloup <thomas.chiloup@WS-MFO-PRETZ600>
2019-06-13 13:27:20 +0200

    Ajoutez des fichiers projet.

---
+++ CorrespondantsMedicaux/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CorrespondantsMedicaux.Startup))]
+namespace CorrespondantsMedicaux
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
